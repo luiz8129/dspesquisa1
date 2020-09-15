c8f7b4f14f0cf5c2a13b0aa3b6fd66fd6e920a45
luiz8129 <luizinhospider@hotmail.com>
2020-09-15 13:44:25 -0300

    project created

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/Dspesquisa1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dspesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dspesquisa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dspesquisa1Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dspesquisa/Dspesquisa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dspesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dspesquisa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
